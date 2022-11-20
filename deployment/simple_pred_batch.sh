c50da51d380b97f37191820bcb0d992efe9e20de
Xav <15103036+XAV-01@users.noreply.github.com>
2022-11-20 15:46:21 +0100

    add batch file

---
+++ deployment/simple_pred_batch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "python deploy_code_package_with_input.py --yaml_file configuration_simple_pred_batch.yaml --test_run True"
+python deploy_code_package_with_input.py --yaml_file configuration_simple_pred_batch.yaml --test_run True
---
+++ deployment/simple_pred_online.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "python deploy_code_package_with_input.py --yaml_file configuration_simple_pred_online.yaml --test_run True"
+python deploy_code_package_with_input.py --yaml_file configuration_simple_pred_online.yaml --test_run True
